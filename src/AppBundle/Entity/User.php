5aa7678833b6aa3a1a55a0d093845545c1dea781
pablogomezar <pablo_gnr@live.com.ar>
2018-11-13 17:22:46 -0300

    hola

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,6 +19,50 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+     protected $name;
+
+     public function getName(){
+       return $this->name;
+     }
+
+     public function setName($n){
+       $this->name=$n;
+     }
+
+     /**
+      * @ORM\Column(type="string", length=255)
+      *
+      * @Assert\NotBlank(message="Please enter your lastname.", groups={"Registration", "Profile"})
+      * @Assert\Length(
+      *     min=3,
+      *     max=255,
+      *     minMessage="The lastname is too short.",
+      *     maxMessage="The lastname is too long.",
+      *     groups={"Registration", "Profile"}
+      * )
+      */
+      protected $lastname;
+
+      public function getLastname(){
+        return $this->lastname;
+      }
+
+      public function setLastname($l){
+        $this->lastname=$l;
+      }
 
     public function __construct()
     {
